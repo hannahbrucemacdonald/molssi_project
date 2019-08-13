fa938e9ef3122cdb26276fa357694b293858497b
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2019-08-13 11:56:19 -0400

    adding tests for failures

--- molssi_project/math.py
+++ molssi_project/math.py
@@ -6,6 +6,8 @@ A file for executing maths.
 import numpy as np
 
 def euler( n=10):
+    if n < 0:
+        raise ValueError("Only positive integers are allowed")
     series = [] 
     for x in range(0,n):
         series.append( 1./(np.math.factorial(x)) )
--- molssi_project/tests/test_math.py
+++ molssi_project/tests/test_math.py
@@ -4,3 +4,8 @@ import molssi_project as mp
 @pytest.mark.parametrize('n, answer', [(0,0.), (1, 1.), (2, 2.), (3, 2.5),(4,2.667)])
 def test_euler(n, answer):
     assert (mp.math.euler(n) == pytest.approx(answer, abs=2)), 'Euler function for {n} does not match expected answer {answer}'
+
+
+def test_euler_failures():
+    with pytest.raises(ValueError):
+        mp.math.euler(-1)

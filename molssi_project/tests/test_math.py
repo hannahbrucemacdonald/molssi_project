4401efaa6a52f1512c17ef4341f93ec97a37b1e9
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2019-08-13 11:44:18 -0400

    adding tolerance to euler test

--- molssi_project/tests/test_math.py
+++ molssi_project/tests/test_math.py
@@ -1,6 +1,6 @@
 import pytest
 import molssi_project as mp
 
-@pytest.mark.parametrize('n, answer', [(0,0.), (1, 1.), (2, 2.), (3, 2.5)])
+@pytest.mark.parametrize('n, answer', [(0,0.), (1, 1.), (2, 2.), (3, 2.5),(4,2.667)])
 def test_euler(n, answer):
-    assert (mp.math.euler(n) == answer), 'Euler function for {n} does not match expected answer {answer}'
+    assert (mp.math.euler(n) == pytest.approx(answer, abs=2)), 'Euler function for {n} does not match expected answer {answer}'
